f8af355db99d4b41a42dbfddada8831c38495470
Laura <l@veriny.tf>
2017-03-12 22:46:13 +0000

    Use LWGJL3 for the desktop backend.

--- desktop/src/tf/veriny/kl27/desktop/DesktopLauncher.kt
+++ desktop/src/tf/veriny/kl27/desktop/DesktopLauncher.kt
@@ -1,16 +1,15 @@
 package tf.veriny.kl27.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import tf.veriny.kl27.KL27
 
 object DesktopLauncher {
     @JvmStatic fun main(arg: Array<String>) {
-        val config = LwjglApplicationConfiguration()
+        val config = Lwjgl3ApplicationConfiguration()
         // we need to disable vsync so that the CPU can run a lot faster
-        config.vSyncEnabled = false
-        config.foregroundFPS = 0
-        config.backgroundFPS = 0
-        LwjglApplication(KL27("/home/laura/dev/kl27/compiler/testfile.k27"), config)
+        config.useVsync(false)
+        config.setIdleFPS(0)
+        Lwjgl3Application(KL27("/home/laura/dev/kl27/compiler/testfile.k27"), config)
     }
 }
